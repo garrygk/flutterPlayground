bc2191a1fd27219df417bfeedcc668d63a5b5a71
Garry <garry@backbase.com>
2024-01-03 00:37:15 +0100

    made a bunch of changes to the modal. expenses and such.

--- udemy_course/expense_tracker/lib/main.dart
+++ udemy_course/expense_tracker/lib/main.dart
@@ -5,8 +5,8 @@ import 'package:expense_tracker/widgets/expenses.dart';
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: Expenses(),
+      theme: ThemeData().copyWith(useMaterial3: true),
+      home: const Expenses(),
     ),
   );
 }
