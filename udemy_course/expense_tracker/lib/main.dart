a352db16f931d29748f4cc345a9c61a19efc060a
Garry <garry@backbase.com>
2024-01-04 22:19:37 +0100

    finished up the app. added dark mode. added charts.

--- udemy_course/expense_tracker/lib/main.dart
+++ udemy_course/expense_tracker/lib/main.dart
@@ -2,10 +2,58 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColourScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 216, 126, 224)
+  );
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 81, 154, 214),
+  brightness: Brightness.dark
+  );
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData().copyWith(useMaterial3: true),
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer
+          ),
+        ),
+      ),
+
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColourScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColourScheme.onPrimaryContainer,
+          foregroundColor: kColourScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColourScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColourScheme.primaryContainer
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColourScheme.onSecondaryContainer,
+            fontSize: 16
+          )        )
+      ),
+      themeMode: ThemeMode.system,
       home: const Expenses(),
     ),
   );
--- udemy_course/expense_tracker/lib/models/expense.dart
+++ udemy_course/expense_tracker/lib/models/expense.dart
@@ -28,5 +28,29 @@ class Expense {
   String get formattedDate {
     return formatter.format(date);
   }
+}
+
+class ExpenseBucket {
+
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+  
+  // util function to create different buckets that are represented by chart bars in our chart
+  ExpenseBucket.forCategory(List<Expense> allExpense, this.category) : expenses = allExpense.where((expense) => expense.category == category).toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses{
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
 
 }
