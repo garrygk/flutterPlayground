ad00b91c8182d2400b6a299a1e405e8f9f5fcd54
Garry <garry@backbase.com>
2024-01-12 00:05:36 +0100

    completed meals screens. add bottom nav bar.

--- udemy_course/meals/lib/screens/meals.dart
+++ udemy_course/meals/lib/screens/meals.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meal_details.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, required this.title, required this.meals});
+  const MealsScreen({super.key, this.title, required this.meals});
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
+
+  void _selectedMeal(BuildContext context, Meal meal){
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (ctx) => MealDetailsScreen(meal: meal,)
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
 
     Widget content = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: (ctx, index) => Text(meals[index].title)
+      itemBuilder: (ctx, index) => MealItem(meal: meals[index], onSelectedMeal: () => _selectedMeal(context, meals[index])
+      )
     );
 
     if (meals.isEmpty) {
@@ -23,8 +32,12 @@ class MealsScreen extends StatelessWidget {
       ],),);
     }
 
+    if (title == null){
+      return content;
+    }
+
     return Scaffold(
-      appBar: AppBar(title: Text(title),),
+      appBar: AppBar(title: Text(title!),),
       body: content,
     );
   }
