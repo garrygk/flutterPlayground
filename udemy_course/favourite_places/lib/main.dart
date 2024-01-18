5a8c4c427d7b0a556ef6886478e197cc7b181d7f
Garry <garry@backbase.com>
2024-01-19 00:14:15 +0100

    started the location app. added the photo and location libraries. Did a bunch of things

--- udemy_course/favourite_places/lib/main.dart
+++ udemy_course/favourite_places/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:favourite_places/models/place.dart';
+import 'package:favourite_places/screens/places.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final colorScheme = ColorScheme.fromSeed(
@@ -27,7 +29,7 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-      const MyApp(),
+      const ProviderScope(child: MyApp()),
   );
 }
 
@@ -39,7 +41,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Great Places',
       theme: theme,
-      home: ...,
+      home: const PlacesScreen(),
     );
   }
 }
